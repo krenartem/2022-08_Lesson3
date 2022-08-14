8d6d23e89255983f06e0d5f650a6f97f9a29422c
krenartem <krenartem@yandex.ru>
2022-08-14 20:32:35 +0300

    added homework task 019

--- HW_task019/HW_task019.cs
+++ HW_task019/HW_task019.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberFirst = number;
+int pow = 4;
+double numberPalindrome = 0;
+if (number < 10000 ^ number > 99999)
+{
+    Console.WriteLine($"Число {number} не пятизначное");
+}
+else
+{
+    while (pow > -1)
+    {
+        numberPalindrome = numberPalindrome + number % 10 * Math.Pow(10, pow);
+        number = number / 10;
+        pow = pow - 1;
+    }
+    if (numberFirst==numberPalindrome)
+    {
+        Console.WriteLine($"УРА! Число {numberFirst} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Жаль, но число {numberFirst} не палиндром");
+    }
+}
--- HW_task023/HW_task023.cs
+++ HW_task023/HW_task023.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int pow = 4;
+int numberPalindrome;
+if (number < 10000 ^ number > 99999)
+{
+    Console.WriteLine($"Число {number} не пятизначное");
+}
+else
+{
+    while (pow > 0)
+    {
+        numberPalindrome = numberPalindrome + number % 10 * Math.Pow(10, pow);
+        number = number / 10;
+    }
+    Console.Writeline(numberPalindrome);
+}
