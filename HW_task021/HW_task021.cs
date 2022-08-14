11e91f0099ee827beee19034f9ea1b08a258e219
krenartem <krenartem@yandex.ru>
2022-08-14 20:56:44 +0300

    added homework task 021

--- HW_task021/HW_task021.cs
+++ HW_task021/HW_task021.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введите 3D координаты первой точки:");
+Console.Write("X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3D координаты второй точки:");
+Console.Write("X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double range = Math.Round(Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2)), 2);
+Console.WriteLine($"Расстояние между точками 1 и 2 = {range}");
