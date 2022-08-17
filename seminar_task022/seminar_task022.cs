a490cee735c91dd24713fe83fa4a54711bc00514
krenartem <krenartem@yandex.ru>
2022-08-17 23:08:06 +0300

    add selfmade seminar task 022 by 'method' & cycles 'while' and 'for'

---
+++ seminar_task022/seminar_task022.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число N и выдает таблицу квадратов чисел от 1 до N
+void Square(int a)
+{
+    int aSquared = a * a;
+    Console.Write($"{aSquared} ");
+}
+int N = new Random().Next(1, 100);
+Console.WriteLine($"для случайного числа {N} квадраты целых чисел от 1 до {N}:");
+int i = 1;
+// while (i <= N)
+// {
+//     Square(i);
+//     i++;
+// }
+// Console.WriteLine();
+for (i = 1; i <= N; i++)
+{
+    Square(i);
+}
+Console.WriteLine();
