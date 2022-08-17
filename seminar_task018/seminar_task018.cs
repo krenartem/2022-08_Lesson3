5fbd6cbfc4375e5fa48e12cda0ea609c61446ea4
krenartem <krenartem@yandex.ru>
2022-08-17 22:06:59 +0300

    add selfmade seminar task 018

---
+++ seminar_task018/seminar_task018.cs
@@ -0,0 +1,25 @@
+﻿// по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y)
+void coordinates(int number)
+{
+    switch (number)
+    {
+        case 1: Console.WriteLine($"в четверти {number} координаты: x > 0, y > 0"); break;
+        case 2: Console.WriteLine($"в четверти {number} координаты: x > 0, y < 0"); break;
+        case 3: Console.WriteLine($"в четверти {number} координаты: x < 0, y < 0"); break;
+        case 4: Console.WriteLine($"в четверти {number} координаты: x < 0, y > 0"); break;
+        default: Console.WriteLine($"введенное значение {number} не входит в диапазон от 1 до 4"); break;
+    }
+}
+Console.Clear();
+Console.WriteLine("    ^    ");
+Console.WriteLine("    |    ");
+Console.WriteLine("  4 | 1  ");
+Console.WriteLine("    |    ");
+Console.WriteLine("-------->");
+Console.WriteLine("    |    ");
+Console.WriteLine("  3 | 2  ");
+Console.WriteLine("    |    ");
+Console.WriteLine();
+Console.Write("введите номер четверти: ");
+int quarter = int.Parse(Console.ReadLine());
+coordinates(quarter);
